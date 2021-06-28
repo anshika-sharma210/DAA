71b48cc9ad8bda0b5d019c3a0e43bd6cc1c168ed
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2021-06-28 01:39:29 -0700

    Add files via upload

---
+++ DAA programArray 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int empID[5];
+	empID[0]=1038;
+	empID[1]=2365;
+	empID[2]=4534;
+	empID[3]=4267;
+	for(i=0;i<=4;i++) 
+	{
+		printf("%d n, empID[i]");
+		}
+		return 0;
+}
